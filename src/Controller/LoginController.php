88e2d18662ab5d9aed1acac5724f21c95795f237
user <kasyanova.b@effective-group.ru>
2022-04-12 12:35:00 +0300

    Формы работают + базовый внешний вид

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,7 +6,7 @@ use EfTech\SportClub\Form\LoginForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Throwable;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
  * Класс обработки авторизации
@@ -15,93 +15,23 @@ class LoginController extends AbstractController
 {
     /**
      * @param Request $serverRequest
+     * @param AuthenticationUtils $utils
      * @return Response
      */
-    public function __invoke(Request $serverRequest): Response
+    public function __invoke(
+        Request $serverRequest,
+        AuthenticationUtils $utils
+    ): Response
     {
-        try {
-            $response = $this->doLogin($serverRequest);
-        } catch (Throwable $e) {
-            $response = $this->buildErrorResponse($e);
-        }
-        return $response;
-    }
-
-    /**
-     * @param  Throwable $e
-     * @return Response
-     */
-    private function buildErrorResponse(Throwable $e): Response
-    {
-        $httpCode = 500;
-        $template = 'errors.twig';
-        $context  = [
-            'errors' => [$e->getMessage()],
-        ];
-        $response = $this->render($template, $context);
-        $response->setStatusCode($httpCode);
-        return $response;
-    }
-
-    /**
-     * Реализация процесса аутентификации
-     *
-     * @param  Request $serverRequest
-     * @return Response
-     */
-    private function doLogin(Request $serverRequest): Response
-    {
+        $errs = $utils->getLastAuthenticationError();
         $formLogin = $this->createForm(LoginForm::class);
-        $formLogin->handleRequest($serverRequest);
-        $response = null;
-        $context  = [
-            'form_login' => $formLogin
+        $formLogin->setData([
+            'login' => $utils->getLastUsername()
+        ]);
+        $context = [
+            'form_login' => $formLogin,
+            'errs'       => $errs
         ];
-        if ($formLogin->isSubmitted() && $formLogin->isValid()) {
-            $authData = $formLogin->getData();
-            if ($this->isAuth($authData['login'], $authData['password'])) {
-                $response = $serverRequest->query->has('redirect')
-                    ? $this->redirect($serverRequest->query->get('redirect'))
-                    : $this->redirect('/');
-            } else {
-                $context['errMsg'] = 'Логин и пароль не подходят';
-            }
-        }
-        if (null === $response) {
-            $response = $this->renderForm('login.twig', $context);
-        }
-        return $response;
-    }
-
-//    private function validateAuthData(array $authData): void
-//    {
-//        if (false === array_key_exists('login', $authData)) {
-//            throw new RuntimeException('Отсутствует логин');
-//        }
-//
-//        if (false === is_string($authData['login'])) {
-//            throw new RuntimeException('Логин имеет неверный формат');
-//        }
-//
-//        if (false === array_key_exists('password', $authData)) {
-//            throw new RuntimeException('Отсутствует пароль');
-//        }
-//
-//        if (false === is_string($authData['password'])) {
-//            throw new RuntimeException('Пароль имеет неверный формат');
-//        }
-//    }
-
-    /**
-     * Проводит аутентификацию пользователя
-     *
-     * @param  string $login    - логин пользователя
-     * @param  string $password - пароль пользователя
-     * @return boolean
-     */
-    private function isAuth(string $login, string $password): bool
-    {
-//        return $this->httpAuthProvider->auth($login, $password);
-        return true;
+        return $this->renderForm('login.twig', $context);
     }
 }
